c26c315b7ca9db671b6180492a3fa89fde38525c
Georgie <georgiehackett47@gmail.com>
2024-02-15 23:25:58 +0000

    Create li elements for scores and append to highscores page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,2 +1,20 @@
-var highscores = localStorage.getItem("highscores");
-console.log(JSON.parse(highscores));
+window.addEventListener("DOMContentLoaded", displayScores);
+
+  var highscoresEl = document.getElementById("highscores");
+  console.log(highscoresEl);
+
+  function displayScores() {
+    console.log("howdy");
+    var highscores = JSON.parse(localStorage.getItem("highscores"));
+    console.log(highscores);
+
+    highscores.forEach((score) => {
+      console.log(score);
+      var scoreEl = document.createElement("li");
+      scoreEl.innerText = `${score.initials}: ${score.score}`;
+      highscoresEl.appendChild(scoreEl);
+    });
+
+    // displayScores();
+  }
+
